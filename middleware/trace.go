eb86b0a7c5f20305e835d5ae03b82d228ae3c174
wb_liubeiluo <wb_liubeiluo@kuaishou.com>
2025-09-05 22:37:21 +0800

    style(tracing middleware): 在跟踪中间件中添加空行 - 在middleware/trace.go文件的WithTraceID函数里添加了一个空行，不影响功能 [middleware/trace.go]

--- middleware/trace.go
+++ middleware/trace.go
@@ -17,6 +17,7 @@ func WithTraceID() gin.HandlerFunc {
 			traceID = time.Now().Format("20060102150405") + "-" + uuid.New().String()
 			c.Request.Header.Set(constants.HttpTraceID, traceID)
 		}
+		
 		// 在响应头中也返回跟踪ID
 		c.Header(constants.HttpTraceID, traceID)
 		// 将跟踪ID存储在gin.Context中，这样可以直接通过context获取
